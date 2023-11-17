5f925f70ab946109abff5f2dda89ba95e117e800
Recep Yıldız <148587672+RecoYldz58@users.noreply.github.com>
2023-11-17 21:19:30 +0300

    hx711kalibretest1.ino

--- hx711kalibretest1.ino
+++ hx711kalibretest1.ino
@@ -8,28 +8,33 @@
  * (c) 2018 Bogdan Necula
  *
 **/
+//---------------------------------- Burda Kütüphaneleri çağıyor
 //include "deneyap.h" //olmaşşa "//" ları sil (1.adım)
 #include "HX711.h"
+//----------------------------------------
 
+
+//---------------------------------------------Bura Pinleri ayarlıyor
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 3; //Olmaşşa "//" yaz başlarına (2.adım)
 const int LOADCELL_SCK_PIN = 5; //Olmaşşa "//" yaz başlarına (2.adım)
+//------------------------------------------------------
 
 HX711 scale;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //Konuşmayı başlatıyor
   scale.begin(LOADCELL_DOUT_PIN,LOADCELL_SCK_PIN); //Olmaşşa D3,D5 olarak değiştir (2.adım)
-  scale.set_scale(0.3925); //yanlışşa birkez daha test et ve bu förmülü uygula "değer = ölçülen değer/sabit değer" (3.adım)
-  scale.tare();      
+  scale.set_scale(0); // Bunu faktör=ölçülen değer/sabit olan değer ile bul ve buraya yaz Ve Burda o tartıyı ayarlıyor
+  scale.tare();  
 }
 
 void loop() {
   Serial.print("Weight:");
   
-  Serial.println(scale.get_units(10), 1);
+  Serial.println(scale.get_units(10), 1); //Burda ağırlığı yazıyor formüllenmiş halini eğer hala ağırlık doğru değilse tekrardan test et ve formülü uygula
 
-  scale.power_down();
+  scale.power_down(); //enerjiyi sensöre azaltıyor
   delay(1000);
-  scale.power_up();
+  scale.power_up(); //enerjiyi sensöre tekrardan veriyor 
 }
